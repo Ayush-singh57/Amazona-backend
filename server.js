0d47fa597e2d23f3b0abd07e16c0a4dc21785d41
Ayush <ayushchouhan401@gmail.com>
2026-05-12 02:01:47 +0530

    .

--- server.js
+++ server.js
@@ -8,8 +8,17 @@ import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
 import uploadRouter from "./routes/uploadRoutes.js";
+const express = require('express');
+const cors = require('cors');
 
 dotenv.config();
+
+const app = express();
+
+// 2. Add the CORS middleware right after 'app = express()'
+app.use(cors({
+    origin: '*' // This allows all domains. For strict security, you can use 'https://d2l4pr350wqghp.cloudfront.net'
+}));
 
 mongoose
   .connect(process.env.MONGODB_URI)
