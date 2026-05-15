50f077be9964a15e2b0c79c25c1d3ca9d0f6c88e
Ayush <ayushchouhan401@gmail.com>
2026-05-15 21:54:59 +0530

    .

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,7 +4,6 @@ import Order from '../models/orderModel.js';
 import User from '../models/userModel.js';
 import Product from '../models/productModel.js';
 import { isAuth, isAdmin, mailgun, payOrderEmailTemplate } from '../utils.js';
-
 const orderRouter = express.Router();
 
 orderRouter.get(
